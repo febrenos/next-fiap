f1fd2711ee8461e7c1fe69464c99b4de70e11c24
febreno <felipesugisawa1@gmail.com>
2023-05-02 17:17:18 -0300

    new

--- front/src/components/Btn/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import './style.css'
-
-export default ({background, text, borderColor, size}) => {
-    return(
-        <button style={{background:background, borderColor:borderColor}} className={`btn ${size}`}>{text}</button>
-    )
-}
